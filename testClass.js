e3a7aa200c1e3764cf70de3845df309beba09c3d
Jade Taboada <jadeonprog@gmail.com>
2021-12-02 12:10:57 +0800

    initial commit for main

---
+++ testClass.js
@@ -0,0 +1,23 @@
+Class TestClass {
+
+	function reverse(number) {
+	 	let digit, result = 0
+
+		while( number ){
+			digit = number % 10;  
+			result = (result * 10) + digit;
+			number = number/10|0; 
+		}  
+	  
+		return result
+	}
+
+	function fizzBuzz(limit=10) {
+
+		for(var i=1;i<=limit;i++){
+		   console.log(((['Fizz'][i%3] || '') + (['Buzz'][i%5] || '')) || i)
+		} 
+		
+		return;
+	}
+}
